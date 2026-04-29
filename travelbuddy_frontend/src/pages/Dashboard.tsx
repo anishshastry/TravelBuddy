cb047b4ca270a75a3e9ba8c1c126664f31295eb5
anishshastry <99903802+anishshastry@users.noreply.github.com>
2026-04-29 22:07:16 +0530

    Delete travelbuddy_frontend directory

--- travelbuddy_frontend/src/pages/Dashboard.tsx
+++
@@ -1,8 +0,0 @@
-export default function Dashboard() {
-  return (
-    <div style={{ padding: "40px", textAlign: "center" }}>
-      <h1>Welcome to Dashboard</h1>
-      <p>Login successful..!!</p>
-    </div>
-  );
-}
